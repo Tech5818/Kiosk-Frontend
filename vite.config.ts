448c61728359d8e8016cfdea25d49621a974bc01
Tech5818 <pqow5818@gmail.com>
2024-11-28 08:40:11 +0900

    Comment: Modified tsconfig Alias & vite config Alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import tsconfig from "vite-tsconfig-paths";
+import tsconfig from "vite-tsconfig-paths"; // Setting Alias
 
 // https://vite.dev/config/
 export default defineConfig({
